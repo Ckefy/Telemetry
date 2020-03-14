286a21d2654110c960dfd90c9d0d04635a9758ba
Lukonin Arseny <lukonin004@gmail.com>
2020-03-14 22:32:22 +0300

    Main module

--- telemetry.cpp
+++ telemetry.cpp
@@ -1,3 +1,5 @@
+#include "Graphic.h"
+
 #include <iostream>
 #include <algorithm>
 #include <fstream>
@@ -7,155 +9,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
-class Data {
-public:
-
-	void parse_data(const std::string& data) {
-		std::vector<std::string> parsed;
-		std::istringstream iss(data);
-		for (std::string s; iss >> s;) {
-			std::replace(s.begin(), s.end(), ',', '.');
-			parsed.push_back(s);
-		}
-		if (parsed.size() < 12) {
-			throw "ERROR: There are less information than expected!\nExpected format: \
-					\nDate\nName\nAzimuth\nTangage\nAngle\nLatitude of observer\nLongitude of observer \
-					\nLatitude of aim\nLongitude of aim\nDistance\nHeight of GPS";
-		}
-		this->date = parse_date_format(parsed[0]) + " " + parsed[1];
-		this->name = parsed[2];
-		this->azimuth = parsed[3];
-		this->tangage = parsed[4];
-		this->angle = parsed[5];
-		this->latitude_observer = parse_coordinates(parsed[6]);
-		this->longitude_observer = parse_coordinates(parsed[7]);
-		this->latitude_aim = parse_coordinates(parsed[8]);
-		this->longitude_aim = parse_coordinates(parsed[9]);
-		this->distance = parsed[10] + "m";
-		this->height_GPS = parsed[11];
-	}
-
-	std::string date;
-	std::string name;
-	std::string azimuth;
-	std::string tangage;
-	std::string angle;
-	std::string latitude_observer;
-	std::string longitude_observer;
-	std::string latitude_aim;
-	std::string longitude_aim;
-	std::string distance;
-	std::string height_GPS;
-
-private:
-
-	std::string parse_date_format(std::string date) {
-		std::vector <std::string> formated_date;
-		int index = 0;
-		std::string temp = "";
-		while (index < date.length()) {
-			if (date[index] == '.' || date[index] == '-') {
-				formated_date.push_back(temp);
-				temp = "";
-				index++;
-				continue;
-			}
-			temp += date[index];
-			index++;
-		}
-		formated_date.push_back(temp);
-		if (formated_date[0].length() > 2) {
-			return formated_date[2] + "." + formated_date[1] + "." + formated_date[0];
-		}
-		else {
-			return formated_date[0] + "." + formated_date[1] + "." + formated_date[2];
-		}
-	}
-
-	std::string parse_coordinates(std::string coord) {
-		bool minus = coord[0] == '-' ? true : false;
-		int degree = stoi(coord.substr(0, coord.find('.')));
-		double mod = stod("0." + coord.substr(coord.find('.') + 1));
-		std::string minutes = std::to_string(mod * 60);
-		double seconds = stod("0." + minutes.substr(minutes.find('.') + 1)) * 60;
-		int roundSeconds = int(seconds);
-		if (seconds - roundSeconds > 0.5) {
-			roundSeconds++;
-		}
-		minutes = minutes.substr(0, minutes.find('.'));
-		if (minutes.length() < 2) {
-			minutes = "0" + minutes;
-		}
-		return (minus ? "-" : "+") + std::to_string(degree) + "." + minutes + "." + std::to_string(roundSeconds);
-	}
-};
-
-class Graphic {
-public:
-
-	Graphic(std::string source) {
-		this->image = cv::imread(source).clone();
-		if (image.empty()) {
-			throw "ERROR: No image with such name in direction!";
-		}
-	}
-
-	void resize_resolution(int horizontal, int vertical) {
-		cv::Size size(horizontal, vertical);
-		cv::resize(image, image, size);
-		this->SCREEN_WIDTH = horizontal;
-		this->SCREEN_HEIGHT = vertical;
-		this->THICKNESS = int(10 * (double(SCREEN_WIDTH) / 6000));
-		this->FONT_SCALE = 3.5 * (double(SCREEN_WIDTH) / 6000);
-		this->DATE_POSITION = cv::Point2f(SCREEN_WIDTH * 0.03, SCREEN_HEIGHT * 0.94);
-		this->DISTANCE_POSITION = cv::Point2f(SCREEN_WIDTH * 0.9, SCREEN_HEIGHT * 0.05);
-		this->LATITUDE_AIM_POSITION = cv::Point2f(SCREEN_WIDTH * 0.72, SCREEN_HEIGHT * 0.90);
-		this->LONGITUDE_AIM_POSITION = cv::Point2f(SCREEN_WIDTH * 0.85, SCREEN_HEIGHT * 0.90);
-		this->LATITUDE_OBSERVER_POSITION = cv::Point2f(SCREEN_WIDTH * 0.72, SCREEN_HEIGHT * 0.95);
-		this->LONGITUDE_OBSERVER_POSITION = cv::Point2f(SCREEN_WIDTH * 0.85, SCREEN_HEIGHT * 0.95);
-	}
-
-	void print(Data data) {
-		print_text(data.date, this->DATE_POSITION);
-		print_text(data.distance, this->DISTANCE_POSITION);
-		print_text(data.latitude_aim, this->LATITUDE_AIM_POSITION);
-		print_text(data.longitude_aim, this->LONGITUDE_AIM_POSITION);
-		print_text(data.latitude_observer, this->LATITUDE_OBSERVER_POSITION);
-		print_text(data.longitude_observer, this->LONGITUDE_OBSERVER_POSITION);
-
-	}
-
-	cv::Mat getImage() {
-		return this->image;
-	}
-
-private:
-
-	cv::Mat image;
-
-	void print_text(std::string text, cv::Point text_position) {
-		cv::Size text_size = cv::getTextSize(text, FONT, FONT_SCALE, THICKNESS, 0);
-		cv::Point bottom_left = cv::Point2f(text_position.x, text_position.y);
-		cv::Point top_right = cv::Point2f(text_position.x + text_size.width, text_position.y - text_size.height);
-		cv::rectangle(image, bottom_left, top_right, cv::Scalar(0, 0, 0), -1, 8, 0); //-1 to fill rectangle
-		cv::putText(image, text, text_position, FONT, FONT_SCALE, TEXT_COLOR, THICKNESS, LINE_TYPE);
-	}
-
-	int SCREEN_WIDTH;
-	int SCREEN_HEIGHT;
-	int FONT = cv::FONT_HERSHEY_SIMPLEX;
-	double FONT_SCALE;
-	cv::Scalar TEXT_COLOR = cv::Scalar(255, 255, 255);
-	int THICKNESS;
-	int LINE_TYPE = 8;
-	cv::Point DATE_POSITION;
-	cv::Point DISTANCE_POSITION;
-	cv::Point LATITUDE_AIM_POSITION;
-	cv::Point LONGITUDE_AIM_POSITION;
-	cv::Point LATITUDE_OBSERVER_POSITION;
-	cv::Point LONGITUDE_OBSERVER_POSITION;
-
-};
+const int STANDART_WIDTH = 6000;
+const int STANDART_HEIGHT = 4000;
+const int SMALL_WIDTH = 3000;
+const int SMALL_HEIGHT = 2000;
 
 int main() {
 	Data data;
@@ -184,11 +41,11 @@ int main() {
 		input.close();
 
 		Graphic graphic(image);
-		if (graphic.getImage().cols < 3000) {
-			graphic.resize_resolution(3000, 2000);
+		if (graphic.getImage().cols < SMALL_WIDTH) {
+			graphic.resize_resolution(SMALL_WIDTH, SMALL_HEIGHT);
 		}
 		else {
-			graphic.resize_resolution(6000, 4000);
+			graphic.resize_resolution(STANDART_WIDTH, STANDART_HEIGHT);
 		}
 		graphic.print(data);
 		imwrite(image.substr(0, image.find('.')) + "_result.JPG", graphic.getImage());
