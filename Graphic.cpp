e5467e7a0d320e983ff8ac4ff02b263cfb2bc57c
LisLiss <47861039+LisLiss@users.noreply.github.com>
2020-03-14 23:17:51 +0300

    Const fixed

--- Graphic.cpp
+++ Graphic.cpp
@@ -13,14 +13,14 @@ void Graphic::resize_resolution(int horizontal, int vertical) {
 	cv::resize(image, image, size);
 	this->SCREEN_WIDTH = horizontal;
 	this->SCREEN_HEIGHT = vertical;
-	this->THICKNESS = int(10 * (double(SCREEN_WIDTH) / 6000));
-	this->FONT_SCALE = 3.5 * (double(SCREEN_WIDTH) / 6000);
-	this->DATE_POSITION = cv::Point2f(SCREEN_WIDTH * 0.03, SCREEN_HEIGHT * 0.94);
-	this->DISTANCE_POSITION = cv::Point2f(SCREEN_WIDTH * 0.9, SCREEN_HEIGHT * 0.05);
-	this->LATITUDE_AIM_POSITION = cv::Point2f(SCREEN_WIDTH * 0.72, SCREEN_HEIGHT * 0.90);
-	this->LONGITUDE_AIM_POSITION = cv::Point2f(SCREEN_WIDTH * 0.85, SCREEN_HEIGHT * 0.90);
-	this->LATITUDE_OBSERVER_POSITION = cv::Point2f(SCREEN_WIDTH * 0.72, SCREEN_HEIGHT * 0.95);
-	this->LONGITUDE_OBSERVER_POSITION = cv::Point2f(SCREEN_WIDTH * 0.85, SCREEN_HEIGHT * 0.95);
+	this->THICKNESS = int(STANDART_THICKNESS * (double(SCREEN_WIDTH) / STANDART_WIDTH));
+	this->FONT_SCALE = STANDART_FONT_SCALE * (double(SCREEN_WIDTH) / STANDART_WIDTH);
+	this->DATE_POSITION = cv::Point2f(SCREEN_WIDTH * DATE_POSITION_PERCENT.first / 100, SCREEN_HEIGHT * DATE_POSITION_PERCENT.second / 100);
+	this->DISTANCE_POSITION = cv::Point2f(SCREEN_WIDTH * DISTANCE_POSITION_PERCENT.first / 100, SCREEN_HEIGHT * DISTANCE_POSITION_PERCENT.second / 100);
+	this->LATITUDE_AIM_POSITION = cv::Point2f(SCREEN_WIDTH * LATITUDE_AIM_POSITION_PERCENT.first / 100, SCREEN_HEIGHT * LATITUDE_AIM_POSITION_PERCENT.second / 100);
+	this->LONGITUDE_AIM_POSITION = cv::Point2f(SCREEN_WIDTH * LONGITUDE_AIM_POSITION_PERCENT.first / 100, SCREEN_HEIGHT * LONGITUDE_AIM_POSITION_PERCENT.second / 100);
+	this->LATITUDE_OBSERVER_POSITION = cv::Point2f(SCREEN_WIDTH * LATITUDE_OBSERVER_POSITION_PERCENT.first / 100, SCREEN_HEIGHT * LATITUDE_OBSERVER_POSITION_PERCENT.second / 100);
+	this->LONGITUDE_OBSERVER_POSITION = cv::Point2f(SCREEN_WIDTH * LONGITUDE_OBSERVER_POSITION_PERCENT.first / 100, SCREEN_HEIGHT * LONGITUDE_OBSERVER_POSITION_PERCENT.second / 100);
 }
 
 void Graphic::print(Data data) {
@@ -42,6 +42,6 @@ void Graphic::print_text(std::string text, cv::Point text_position) {
 	cv::Size text_size = cv::getTextSize(text, FONT, FONT_SCALE, THICKNESS, 0);
 	cv::Point bottom_left = cv::Point2f(text_position.x, text_position.y);
 	cv::Point top_right = cv::Point2f(text_position.x + text_size.width, text_position.y - text_size.height);
-	cv::rectangle(image, bottom_left, top_right, cv::Scalar(0, 0, 0), -1, 8, 0); //-1 to fill rectangle
+	cv::rectangle(image, bottom_left, top_right, RECTANGLE_COLOR, RECTANGLE_THICKNESS, LINE_TYPE, RECTANLGE_SHIFT); 
 	cv::putText(image, text, text_position, FONT, FONT_SCALE, TEXT_COLOR, THICKNESS, LINE_TYPE);
 }
