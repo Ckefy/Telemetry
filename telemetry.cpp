30a647acd4a7a7137b2a9548d43db8b5d3a0f3e1
Lukonin Arseny <lukonin004@gmail.com>
2020-03-19 14:47:35 +0300

    Now you can specify directory with images

--- telemetry.cpp
+++ telemetry.cpp
@@ -9,42 +9,61 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <windows.h>
+
+const int MAX_PATH_LENGTH = 1000;
 
 int main() {
-	Data data;
 	std::string file;
-	std::string image;
-	std::cout << "Write name of source file and image:" << std::endl;
-	std::cin >> file >> image;
+	std::string pathStr;
+	char path[MAX_PATH_LENGTH];
+	std::cout << "Write name of source file and directory with images:" << std::endl;
+	std::cin >> file >> pathStr;
+	strcpy_s(path, (pathStr + "/*.JPG").c_str());
+	WIN32_FIND_DATAA FindFileData;
+	HANDLE handle;
+	handle = FindFirstFileA(path, &FindFileData);
+	if (handle == INVALID_HANDLE_VALUE) {
+		std::cout << "ERROR: Can`t find directory or it is empty!" << std::endl;
+		system("pause");
+		return -1;
+	}
+	std::vector <std::string> images;
+	images.push_back(FindFileData.cFileName);
+	while (FindNextFileA(handle, &FindFileData))
+	{
+		images.push_back(FindFileData.cFileName);
+	}
 	std::ifstream input(file);
-
+	Data data;
 	try {
 		if (!input.is_open()) {
-			throw "ERROR: Could not open the file!";
+			throw "ERROR: Could not open the source file!";
 		}
-		if (!(image.substr(image.find('.') + 1) == "JPG")) {
-			throw "ERROR: Wrong image format! Expected ***.JPG";
-		}
-		std::string data_string;
-		std::getline(input, data_string);
-		while (!input.eof() && data_string.find(image) == std::string::npos) {
+		for (std::string image : images) {
+			std::string data_string;
 			std::getline(input, data_string);
-		}
-		if (data_string.find(image) == std::string::npos) {
-			throw "ERROR: No image with such name in source file!";
-		}
-		data.parse_data(data_string);
-		input.close();
+			while (!input.eof() && data_string.find(image) == std::string::npos) {
+				std::getline(input, data_string);
+			}
+			if (data_string.find(image) == std::string::npos) {
+				throw "ERROR: No image with such name in source file!";
+			}
+			input.clear();
+			input.seekg(0, std::ios::beg);
+			data.parse_data(data_string);
 
-		Graphic graphic(image);
-		if (graphic.getImage().cols < graphic.SMALL_WIDTH) {
-			graphic.resize_resolution(graphic.SMALL_WIDTH, graphic.SMALL_HEIGHT);
+			Graphic graphic(image);
+			if (graphic.getImage().cols < graphic.SMALL_WIDTH) {
+				graphic.resize_resolution(graphic.SMALL_WIDTH, graphic.SMALL_HEIGHT);
+			}
+			else {
+				graphic.resize_resolution(graphic.STANDART_WIDTH, graphic.STANDART_HEIGHT);
+			}
+			graphic.print(data);
+			imwrite(pathStr + "/" + image.substr(0, image.find('.')) + "_result.JPG", graphic.getImage());
 		}
-		else {
-			graphic.resize_resolution(graphic.STANDART_WIDTH, graphic.STANDART_HEIGHT);
-		}
-		graphic.print(data);
-		imwrite(image.substr(0, image.find('.')) + "_result.JPG", graphic.getImage());
+		input.close();
 	}
 	catch (const char* message) {
 		std::cerr << std::endl << message << std::endl << std::endl;
